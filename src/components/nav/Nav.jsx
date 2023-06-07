3e02a7324215e3f3037a8415edd460a5bc739728
Awiral <awiral1234@gmail.com>
2023-06-07 14:03:53 +0545

    ProjectIcon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,6 +4,7 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBookAlt} from 'react-icons/bi'
 import {BiMessageSquareDetail} from 'react-icons/bi'
 import {IoIosSchool} from "react-icons/io"
+import {AiOutlineFundProjectionScreen} from "react-icons/ai"
 import './nav.css'
 import { useState } from 'react'
 const Nav = () => {
@@ -16,6 +17,8 @@ const Nav = () => {
 
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav ==="#experience"? 'active':''}><BiBookAlt/></a>
       <a href="#education" onClick={() => setActiveNav('#education')} className={activeNav ==="#education"? 'active':''} ><IoIosSchool/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav ==="#portfolio"? 'active':''} ><AiOutlineFundProjectionScreen/></a>
+
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav ==="#contact"? 'active':''}><BiMessageSquareDetail/></a>
     </nav>
 
