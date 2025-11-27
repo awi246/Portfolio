c002c4c35446162e1d6f0472f28b7de25999e241
Awiral Chand <103342572+awi246@users.noreply.github.com>
2025-11-28 09:12:43 +1100

    add react-simple-typewriter dependency and implement in About section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,6 +4,8 @@ import ME from "../../assets/main2.jpg";
 import { IoMdSchool } from "react-icons/io";
 import { BsCodeSlash } from "react-icons/bs";
 import { BsAward } from "react-icons/bs";
+import { Typewriter } from "react-simple-typewriter";
+
 const About = () => {
   return (
     <section id="about">
@@ -19,29 +21,47 @@ const About = () => {
           <div className="about__cards">
             <a href="#experience">
               <article className="about__card">
-                <BsCodeSlash className="About__icon" />
+                <BsCodeSlash className="about__icon" />
                 <h5>Coding</h5>
               </article>
             </a>
             <a href="#education">
               <article className="about__card">
-                <IoMdSchool className="About__icon" />
+                <IoMdSchool className="about__icon" />
                 <h5>Education</h5>
               </article>
             </a>
             <a href="#portfolio">
               <article className="about__card">
-                <BsAward className="About__icon" />
+                <BsAward className="about__icon" />
                 <h5>Projects</h5>
               </article>
             </a>
           </div>
           <p>
-            {/* I am a fresher who values qualitative user research and this is
-            something where i feel like i can bring a lot to team. Frontend
-            developer with background knowledge along with a knack of building
-            applications with utmost effciency. */}
-            I am a fresher who values qualitative user research and this is something where i feel like i can bring a lot to team. As a frontend developer, I am committed to staying up-to-date with the latest technologies and trends to provide the best possible solutions to clients. I am excited to start my career and contribute to creating meaningful web applications that make a difference in people's lives.
+            I am a fresher who values qualitative user research and this is
+            something where i feel like i can bring a lot to team. As a frontend
+            developer, I am committed to staying up-to-date with the latest
+            technologies and trends to provide the best possible solutions to
+            clients. I am excited to start my career and contribute to creating
+            meaningful{" "}
+            <span className="typewriter-text">
+              <Typewriter
+                words={[
+                  "web applications",
+                  "user experiences",
+                  "digital solutions",
+                  "innovative projects",
+                ]}
+                loop={0}
+                cursor
+                cursorStyle="|"
+                typeSpeed={70}
+                deleteSpeed={50}
+                delaySpeed={1000}
+              />
+            </span>{" "}
+            that make a difference in people's lives.
           </p>
 
           <a href="#contact" className="btn btn-primary">
